b039fd3534c677c5dc450d52970fd266e6c8e97b
ivasilinets <ivasilinets@gridgain.com>
2015-07-16 13:17:54 +0300

    #ignite-961: fix JSONCacheObject serialVersionUID.

--- modules/core/src/main/java/org/apache/ignite/json/JSONCacheObject.java
+++ modules/core/src/main/java/org/apache/ignite/json/JSONCacheObject.java
@@ -23,6 +23,9 @@ import java.util.*;
  * JSON cache object.
  */
 public class JSONCacheObject extends HashMap<Object, Object> {
+    /** */
+    private static final long serialVersionUID = 0L;
+
     /**
      * Empty constructor.
      */
