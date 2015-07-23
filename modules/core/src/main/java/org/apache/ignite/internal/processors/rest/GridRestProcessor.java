9dd00aca14f38a43ebdc7d6651e5830c6c2f5b6b
ivasilinets <ivasilinets@gridgain.com>
2015-07-23 16:04:39 +0300

    #ignite-961-master: add security check for rest create and destroy cache.

--- modules/core/src/main/java/org/apache/ignite/internal/processors/rest/GridRestProcessor.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/rest/GridRestProcessor.java
@@ -581,6 +581,13 @@ public class GridRestProcessor extends GridProcessorAdapter {
 
                 break;
 
+            case GET_OR_CREATE_CACHE:
+            case DESTROY_CACHE:
+                perm = SecurityPermission.ADMIN_CACHE;
+                name = ((GridRestCacheRequest)req).cacheName();
+
+                break;
+
             case CACHE_METRICS:
             case CACHE_SIZE:
             case TOPOLOGY:
@@ -592,8 +599,6 @@ public class GridRestProcessor extends GridProcessorAdapter {
             case ATOMIC_DECREMENT:
             case NAME:
             case LOG:
-            case GET_OR_CREATE_CACHE:
-            case DESTROY_CACHE:
                 break;
 
             default:
